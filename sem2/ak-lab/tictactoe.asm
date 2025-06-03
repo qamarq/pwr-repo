ede967f60184ac3206692efa14754b789254c6e5
Kamil Marczak <kamilm@you2.pl>
2025-06-03 09:51:13 +0200

    feat(ak): working tictactoe

--- sem2/ak-lab/tictactoe.asm
+++ sem2/ak-lab/tictactoe.asm
@@ -1,407 +1,298 @@
 .data
-prompt_rounds: .asciiz "Podaj liczbe rund (1-5): "
-prompt_move:   .asciiz "Twoj ruch - podaj numer pola (1-9): "
-invalid_move:  .asciiz "Nieprawidlowy ruch, sprobuj ponownie.\n"
-win_human_msg: .asciiz "Wygrales!\n"
-win_comp_msg:  .asciiz "Komputer wygral!\n"
-draw_msg:      .asciiz "Remis!\n"
-final_msg:     .asciiz "Wyniki: Gracz: "
-final_comp:    .asciiz " Komputer: "
-final_draws:   .asciiz " Remisy: "
-newline:       .asciiz "\n"
-char_dot:      .asciiz "."
-char_x:        .asciiz "X"
-char_o:        .asciiz "O"
-char_bar:      .asciiz "|"
+prompt_rounds:   .asciiz "Podaj liczbe rund (1-5): "
+prompt_move:     .asciiz "Twoj ruch. Podaj numer pola (1-9): "
+invalid_move:    .asciiz "Nieprawidlowy ruch. Sprobuj ponownie.\n"
+comp_move_msg:   .asciiz "Ruch komputera...\n"
+newline:         .asciiz "\n"
+dashline:        .asciiz "---+---+---\n"
+pipe:            .asciiz "|"
+space:           .asciiz " "
+win_human_msg:   .asciiz "Wygrales runde!\n"
+win_comp_msg:    .asciiz "Komputer wygral runde!\n"
+draw_msg:        .asciiz "Remis!\n"
+final_score_msg: .asciiz "Wynik koncowy: Ty: "
+score_mid:       .asciiz ", Komp: "
+score_end:       .asciiz ", Remisy: "
+score_newline:   .asciiz "\n"
 
-# tablica 9 pól (s³owa 4?bajtowe)
-board:         .space 36
+num_rounds:      .word   0
+curr_round:      .word   0
+wins_h:          .word   0
+wins_c:          .word   0
+draws:           .word   0
+board:           .space  9
 
-# wzorce zwyciêstwa (8 linii po 3 indeksy)
-win_patterns:
-        .word 0,1,2
-        .word 3,4,5
-        .word 6,7,8
-        .word 0,3,6
-        .word 1,4,7
-        .word 2,5,8
-        .word 0,4,8
-        .word 2,4,6
+# Win conditions (indices of fields)
+winconds: .byte 0,1,2,3,4,5,6,7,8,0,3,6,1,4,7,2,5,8,0,4,8,2,4,6
 
         .text
         .globl main
-
-# ----------------------------------------
-# main: czyta liczbê rund, wywo³uje play_round,
-# zbiera statystyki, na koniec drukuje wynik
-# ----------------------------------------
 main:
-        # zerujemy liczniki
-        li      $s0,0        # human wins
-        li      $s1,0        # comp wins
-        li      $s2,0        # draws
-
+    # Wczytaj liczbe rund (1-5)
+    la    $a0, prompt_rounds
+    li    $v0, 4
+    syscall
 read_rounds:
-        li      $v0,4
-        la      $a0,prompt_rounds
-        syscall
-
-        li      $v0,5        # read int
-        syscall
-        move    $t0,$v0
-        blt     $t0,1,read_rounds
-        bgt     $t0,5,read_rounds
-        move    $t1,$t0      # t1 = liczba rund
-        li      $t2,0        # aktualna runda
+    li    $v0, 5
+    syscall
+    move  $t0, $v0        # t0 = liczba rund
+    li    $t1, 1
+    li    $t2, 5
+    blt   $t0, $t1, read_rounds
+    bgt   $t0, $t2, read_rounds
+    sw    $t0, num_rounds
+    li    $t3, 0
+    sw    $t3, wins_h
+    sw    $t3, wins_c
+    sw    $t3, draws
+    li    $t4, 1
+    sw    $t4, curr_round
 
 round_loop:
-        jal     play_round
-        move    $t3,$v0      # kod zwyciêzcy: 0=remis,1=human,2=comp
-        li      $t4,1
-        beq     $t3,$t4,human_won
-        li      $t4,2
-        beq     $t3,$t4,comp_won
-        # remis
-        addi    $s2,$s2,1
-        j       end_round
+    lw    $t4, curr_round
+    lw    $t5, num_rounds
+    bgt   $t4, $t5, end_rounds
 
-human_won:
-        addi    $s0,$s0,1
-        j       end_round
+    # Inicjuj plansze
+    la    $t6, board
+    li    $t7, 0
+init_board:
+    sb    $zero, 0($t6)
+    addi  $t6, $t6, 1
+    addi  $t7, $t7, 1
+    li    $t8, 9
+    blt   $t7, $t8, init_board
+    li    $t9, 0          # liczba ruchow
 
-comp_won:
-        addi    $s1,$s1,1
+game_loop:
+    # Ruch czlowieka
+    la    $a0, prompt_move
+    li    $v0, 4
+    syscall
+read_move:
+    li    $v0, 5
+    syscall
+    move  $t0, $v0
+    li    $t1, 1
+    li    $t2, 9
+    blt   $t0, $t1, invalid_move_h
+    bgt   $t0, $t2, invalid_move_h
+    la    $t3, board
+    addi  $t0, $t0, -1
+    add   $t3, $t3, $t0
+    lbu   $t4, 0($t3)
+    bne   $t4, $zero, invalid_move_h
+    li    $t5, 1
+    sb    $t5, 0($t3)
+    addi  $t9, $t9, 1
+    jal   print_board
+    jal   check_win
+    move  $t6, $v0        # t6 = wynik
+    li    $t7, 1
+    beq   $t6, $t7, human_wins
+    # Remis?
+    li    $t1, 9
+    beq   $t9, $t1, draw_round
+
+    # Ruch komputera (pierwsze wolne pole)
+    la    $a0, comp_move_msg
+    li    $v0, 4
+    syscall
+    la    $t4, board
+    li    $t5, 0
+find_empty:
+    lbu   $t6, 0($t4)
+    beq   $t6, $zero, place_comp
+    addi  $t4, $t4, 1
+    addi  $t5, $t5, 1
+    li    $t7, 9
+    blt   $t5, $t7, find_empty
+    j     draw_round
+place_comp:
+    li    $t8, 2
+    sb    $t8, 0($t4)
+    addi  $t9, $t9, 1
+    jal   print_board
+    jal   check_win
+    move  $t6, $v0
+    li    $t7, 2
+    beq   $t6, $t7, comp_wins
+    j     game_loop
+
+invalid_move_h:
+    la    $a0, invalid_move
+    li    $v0, 4
+    syscall
+    j     read_move
+
+human_wins:
+    la    $a0, win_human_msg
+    li    $v0, 4
+    syscall
+    lw    $t1, wins_h
+    addi  $t1, $t1, 1
+    sw    $t1, wins_h
+    j     end_round
+
+comp_wins:
+    la    $a0, win_comp_msg
+    li    $v0, 4
+    syscall
+    lw    $t1, wins_c
+    addi  $t1, $t1, 1
+    sw    $t1, wins_c
+    j     end_round
+
+draw_round:
+    la    $a0, draw_msg
+    li    $v0, 4
+    syscall
+    lw    $t1, draws
+    addi  $t1, $t1, 1
+    sw    $t1, draws
 
 end_round:
-        addi    $t2,$t2,1
-        blt     $t2,$t1,round_loop
+    lw    $t2, curr_round
+    addi  $t2, $t2, 1
+    sw    $t2, curr_round
+    j     round_loop
 
-        # drukujemy statystyki
-        li      $v0,4
-        la      $a0,final_msg
-        syscall
+end_rounds:
+    # Wyswietl wynik koncowy
+    la    $a0, final_score_msg
+    li    $v0, 4
+    syscall
+    lw    $a0, wins_h
+    li    $v0, 1
+    syscall
+    la    $a0, score_mid
+    li    $v0, 4
+    syscall
+    lw    $a0, wins_c
+    li    $v0, 1
+    syscall
+    la    $a0, score_end
+    li    $v0, 4
+    syscall
+    lw    $a0, draws
+    li    $v0, 1
+    syscall
+    la    $a0, score_newline
+    li    $v0, 4
+    syscall
+    li    $v0, 10
+    syscall
 
-        li      $v0,1
-        move    $a0,$s0
-        syscall
-
-        li      $v0,4
-        la      $a0,final_comp
-        syscall
-
-        li      $v0,1
-        move    $a0,$s1
-        syscall
-
-        li      $v0,4
-        la      $a0,final_draws
-        syscall
-
-        li      $v0,1
-        move    $a0,$s2
-        syscall
-
-        li      $v0,4
-        la      $a0,newline
-        syscall
-
-        li      $v0,10       # exit
-        syscall
-
-# ----------------------------------------
-# play_round: rozgrywa jedn¹ rundê
-# zwraca w $v0: 1=human,2=comp,0=remis
-# ----------------------------------------
-play_round:
-        addi    $sp,$sp,-4
-        sw      $ra,0($sp)
-
-        jal     init_board
-
-loop_game:
-        # ruch cz³owieka
-        jal     human_move
-        jal     print_board
-
-        jal     check_win
-        li      $t0,1
-        beq     $v0,$t0,ret_human
-
-        jal     board_full
-        bne     $v0,$zero,ret_draw
-
-        # ruch komputera
-        jal     comp_move
-        jal     print_board
-
-        jal     check_win
-        li      $t0,2
-        beq     $v0,$t0,ret_comp
-
-        jal     board_full
-        bne     $v0,$zero,ret_draw
-
-        j       loop_game
-
-ret_human:
-        # komunikat i powrót kodu=1
-        li      $v0,4
-        la      $a0,win_human_msg
-        syscall
-        li      $v0,1
-        move    $a0,$zero   # tu $a0 dowolnie
-        # zwracamy 1
-        li      $v0,1
-        lw      $ra,0($sp)
-        addi    $sp,$sp,4
-        jr      $ra
-
-ret_comp:
-        li      $v0,4
-        la      $a0,win_comp_msg
-        syscall
-        # zwracamy 2
-        li      $v0,2
-        lw      $ra,0($sp)
-        addi    $sp,$sp,4
-        jr      $ra
-
-ret_draw:
-        li      $v0,4
-        la      $a0,draw_msg
-        syscall
-        # zwracamy 0
-        li      $v0,0
-        lw      $ra,0($sp)
-        addi    $sp,$sp,4
-        jr      $ra
-
-# ----------------------------------------
-# init_board: ustawia wszystkie pola na 0
-# ----------------------------------------
-init_board:
-        la      $t0,board
-        li      $t1,9
-init_loop:
-        sw      $zero,0($t0)
-        addi    $t0,$t0,4
-        addi    $t1,$t1,-1
-        bgtz    $t1,init_loop
-        jr      $ra
-
-# ----------------------------------------
-# print_board: drukuje 3x3:
-#  .|X|O
-#  X|.|.
-#  .|O|X
-# ----------------------------------------
+#----------------------------------------
+# Subroutine: print current board
+#----------------------------------------
 print_board:
-        la      $t0,board
-        li      $t1,0        # rz¹d
+    la    $t0, board
+    li    $t1, 0
 row_loop:
-        li      $t2,0        # kolumna
-cell_loop:
-        lw      $t3,0($t0)
-        beq     $t3,$zero,p_dot
-        li      $t4,1
-        beq     $t3,$t4,p_x
-        # w pp. 'O'
-        li      $v0,4
-        la      $a0,char_o
-        syscall
-        j       after_cell
+    li    $t2, 0
+col_loop:
+    # Print leading space
+    la    $a0, space
+    li    $v0, 4
+    syscall
+    # Compute index = row*3 + col
+    mul   $t3, $t1, 3
+    add   $t3, $t3, $t2
+    add   $t4, $t0, $t3
+    lbu   $t5, 0($t4)
+    beq   $t5, $zero, print_empty
+    li    $t6, 1
+    beq   $t5, $t6, print_X
+    # Else print O
+    li    $a0, 79        # 'O'
+    li    $v0, 11
+    syscall
+    j     after_char
+print_X:
+    li    $a0, 88        # 'X'
+    li    $v0, 11
+    syscall
+    j     after_char
+print_empty:
+    la    $a0, space
+    li    $v0, 4
+    syscall
+after_char:
+    # Print trailing space
+    la    $a0, space
+    li    $v0, 4
+    syscall
+    # Print '|' if not last col
+    li    $t6, 2
+    blt   $t2, $t6, pr_pipe
+    j     next_col
+pr_pipe:
+    la    $a0, pipe
+    li    $v0, 4
+    syscall
+next_col:
+    addi  $t2, $t2, 1
+    li    $t6, 3
+    blt   $t2, $t6, col_loop
+    # End row
+    la    $a0, newline
+    li    $v0, 4
+    syscall
+    # Print dash if not last row
+    li    $t6, 2
+    blt   $t1, $t6, pr_dash
+    j     next_row
+pr_dash:
+    la    $a0, dashline
+    li    $v0, 4
+    syscall
+next_row:
+    addi  $t1, $t1, 1
+    li    $t6, 3
+    blt   $t1, $t6, row_loop
+    jr    $ra
 
-p_dot:
-        li      $v0,4
-        la      $a0,char_dot
-        syscall
-        j       after_cell
-
-p_x:
-        li      $v0,4
-        la      $a0,char_x
-        syscall
-
-after_cell:
-        addi    $t2,$t2,1
-        beq     $t2,3,end_cells
-        li      $v0,4
-        la      $a0,char_bar
-        syscall
-        addi    $t0,$t0,4
-        j       cell_loop
-
-end_cells:
-        li      $v0,4
-        la      $a0,newline
-        syscall
-        addi    $t0,$t0,4
-        addi    $t1,$t1,1
-        blt     $t1,3,row_loop
-        jr      $ra
-
-# ----------------------------------------
-# check_win: jeœli ktoœ wygra³ zwraca 1 (human)
-# lub 2 (comp), w pp. 0
-# ----------------------------------------
+#----------------------------------------
+# Subroutine: check for win
+# Returns 1 (X), 2 (O) or 0
+#----------------------------------------
 check_win:
-        la      $t0,win_patterns
-        li      $t1,8       # 8 wzorców
-        li      $t2,1       # marker=1 (human)
+    la    $t0, board       # $t0 = baza planszy
+    li    $t1, 0           # i = 0
+    li    $t2, 8           # liczba warunków = 8
 
-# sprawdzamy dla human
-chk_loop_h:
-        beqz    $t1,chk_comp
-        lw      $s0,0($t0)
-        lw      $s1,4($t0)
-        lw      $s2,8($t0)
-        la      $s3,board
+cw_loop:
+    beq   $t1, $t2, no_win # je¿eli i==8 to koniec, brak wygranej
 
-        sll     $s4,$s0,2
-        add     $s4,$s3,$s4
-        lw      $s5,0($s4)
-        bne     $s5,$t2,next_h
-        sll     $s4,$s1,2
-        add     $s4,$s3,$s4
-        lw      $s5,0($s4)
-        bne     $s5,$t2,next_h
-        sll     $s4,$s2,2
-        add     $s4,$s3,$s4
-        lw      $s5,0($s4)
-        bne     $s5,$t2,next_h
-        # all trzy => human wygrywa
-        li      $v0,1
-        jr      $ra
+    la    $t3, winconds    # baza tablicy warunków
+    mul   $t4, $t1, 3      # t4 = 3*i
+    addu  $t3, $t3, $t4    # t3 = &winconds[3*i]
 
-next_h:
-        addi    $t0,$t0,12
-        addi    $t1,$t1,-1
-        j       chk_loop_h
+    lb    $t4, 0($t3)      # idx1
+    lb    $t5, 1($t3)      # idx2
+    lb    $t6, 2($t3)      # idx3
 
-# teraz comp
-chk_comp:
-        la      $t0,win_patterns
-        li      $t1,8
-        li      $t2,2       # marker=2 (comp)
+    # wczytaj board[idxX]
+    addu  $t7, $t0, $t4
+    lbu   $t7, 0($t7)      # a = board[idx1]
+    addu  $t8, $t0, $t5
+    lbu   $t8, 0($t8)      # b = board[idx2]
+    addu  $t9, $t0, $t6
+    lbu   $t9, 0($t9)      # c = board[idx3]
 
-chk_loop_c:
-        beqz    $t1,no_win
-        lw      $s0,0($t0)
-        lw      $s1,4($t0)
-        lw      $s2,8($t0)
-        la      $s3,board
+    # je¿eli a==b==c i !=0 -> mamy zwyciêzcê
+    bne   $t7, $t8, next_cond
+    bne   $t7, $t9, next_cond
+    beq   $t7, $zero, next_cond
 
-        sll     $s4,$s0,2
-        add     $s4,$s3,$s4
-        lw      $s5,0($s4)
-        bne     $s5,$t2,next_c
-        sll     $s4,$s1,2
-        add     $s4,$s3,$s4
-        lw      $s5,0($s4)
-        bne     $s5,$t2,next_c
-        sll     $s4,$s2,2
-        add     $s4,$s3,$s4
-        lw      $s5,0($s4)
-        bne     $s5,$t2,next_c
-        # comp wygrywa
-        li      $v0,2
-        jr      $ra
+    move  $v0, $t7         # zwyciêzca w $v0
+    jr    $ra
 
-next_c:
-        addi    $t0,$t0,12
-        addi    $t1,$t1,-1
-        j       chk_loop_c
+next_cond:
+    addi  $t1, $t1, 1      # i++
+    j     cw_loop
 
 no_win:
-        li      $v0,0
-        jr      $ra
-
-# ----------------------------------------
-# board_full: 1 jeœli brak wolnych pól, 0 w pp.
-# ----------------------------------------
-board_full:
-        la      $t0,board
-        li      $t1,9
-
-bf_loop:
-        beqz    $t1,bf_full
-        lw      $t2,0($t0)
-        beqz    $t2,bf_not_full
-        addi    $t0,$t0,4
-        addi    $t1,$t1,-1
-        j       bf_loop
-
-bf_full:
-        li      $v0,1
-        jr      $ra
-
-bf_not_full:
-        li      $v0,0
-        jr      $ra
-
-# ----------------------------------------
-# human_move: czyta ruch i zapisuje 1
-# ----------------------------------------
-human_move:
-hm_loop:
-        li      $v0,4
-        la      $a0,prompt_move
-        syscall
-
-        li      $v0,5
-        syscall
-        move    $t0,$v0
-
-        li      $t1,1
-        blt     $t0,$t1,hm_inv
-        li      $t1,9
-        bgt     $t0,$t1,hm_inv
-
-        addi    $t0,$t0,-1
-        sll     $t2,$t0,2
-        la      $t3,board
-        add     $t3,$t3,$t2
-        lw      $t4,0($t3)
-        bne     $t4,$zero,hm_inv
-
-        li      $t5,1
-        sw      $t5,0($t3)
-        jr      $ra
-
-hm_inv:
-        li      $v0,4
-        la      $a0,invalid_move
-        syscall
-        j       hm_loop
-
-# ----------------------------------------
-# comp_move: najpierw œrodek, potem pierwsze
-# wolne pole, zapisuje 2
-# ----------------------------------------
-comp_move:
-        la      $t0,board
-        lw      $t1,16($t0)    # board[4]
-        bne     $t1,$zero,cm_scan
-        li      $t2,2
-        sw      $t2,16($t0)
-        jr      $ra
-
-cm_scan:
-        li      $t3,0
-cm_loop1:
-        beq     $t3,9,cm_end
-        sll     $t4,$t3,2
-        la      $t5,board
-        add     $t5,$t5,$t4
-        lw      $t6,0($t5)
-        bne     $t6,$zero,cm_next
-        li      $t6,2
-        sw      $t6,0($t5)
-        jr      $ra
-
-cm_next:
-        addi    $t3,$t3,1
-        j       cm_loop1
-
-cm_end:
-        jr      $ra
+    li    $v0, 0
+    jr    $ra
